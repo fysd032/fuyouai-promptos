ba599e37f0f984da69ea4fc62d37f498ffd9df25
fysd032 <4635915@gmail.com>
2025-12-15 11:03:53 +0800

    add registry api with cors

---
+++ app/api/registry/route.ts
@@ -0,0 +1,24 @@
+import mapping from "@/module_mapping.v2.json";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "http://localhost:3000",
+  "Access-Control-Allow-Methods": "GET,OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return new Response(null, { status: 200, headers: corsHeaders });
+}
+
+export async function GET() {
+  return new Response(
+    JSON.stringify({ ok: true, version: "v2", data: mapping }, null, 2),
+    {
+      status: 200,
+      headers: {
+        ...corsHeaders,
+        "content-type": "application/json; charset=utf-8",
+      },
+    }
+  );
+}
